32c78bf2846aa6cd418d6271cc3c54e74a960bb0
DenisBakalo <achtech777@gmail.com>
2019-01-15 11:57:35 +0200

    creatposts

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\File;
+use App\Post;
+use Auth;
+class PostsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::where('user_id',Auth::user()->id)->get();
+        return view('home',compact('posts'));
+        
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'title'=>'required',
+            'description'=>'required',
+            'image' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048'
+        ]);
+        $cover = $request->file('image');
+        $extension = $cover->getClientOriginalExtension();
+        Storage::disk('public')->put($cover->getFilename().'.'.$extension,  File::get($cover));
+        $post = new Post();
+        $post->title = $request->title;
+        $post->description = $request->description;
+        $post->user_id = $request->user_id;
+        $post->user_name = $request->user_name;
+        $post->image = $cover->getFilename().'.'.$extension;
+        $post->save(); 
+        return redirect('/home'); 
+    }
+        
+   
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                   {!!Form::open(['route'=>'home.store','files'=>true])!!}
+                        <div class="form-group">
+                             <label >Название статьи</label>
+                             <input type="text" name = "title" class="form-control" placeholder="Введите название статьи"></br>
+                             <input type="hidden" name = "user_id" value = "{{Auth::user()->id}}">
+                             <input type="hidden" name = "user_name" value = "{{Auth::user()->name}} ">
+                             <textarea name="description"></textarea>
+                             </div>
+                            <input name="image"  type="file"></br><hr>
+                            <button type="submit" class="btn btn-success">Отправить</button>
+                   {!!Form::close()!!}
+                   @foreach($posts as $post)
+    {{$post->title}}
+@endforeach
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+</div>
+@endsection
